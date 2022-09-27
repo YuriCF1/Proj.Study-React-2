fe039a3a7a7e765c3880a039a65ebc0013ae337f
Yuri <yurifdev@gmail.com>
2022-09-27 11:17:43 -0300

    Examples what shoudlnt happen

--- hello-world/src/components/Events.js
+++ hello-world/src/components/Events.js
@@ -1,20 +1,18 @@
-const Events =  () => {
-    const handleMyEvent = (event) => {
-        console.log(event);
-        console.log('Cliquei');
+const Events = () => {
+  const handleMyEvent = (event) => {
+    // console.log(event);
+    console.log("Cliquei");
+  };
 
-    }
+  //Se eu colocar parenteses nessa função do onClick, ela será disparada quando o navegador ler o código
+  return (
+    <div>
+      <div>
+        <button onClick={handleMyEvent}>Clique aqui</button>
+      </div>
+      <button onClick={()=>console.log('Clicado de novo')}>Clique aqui também</button>
+    </div>
+  );
+};
 
-    //Se eu colocar parenteses nessa função do onClick, ela será disparada quando o navegador ler o código
-    return (
-        <div>
-            <div>
-                <button onClick={handleMyEvent}>Clique aqui</button> 
-            </div>
-        </div>
-
-    )
-
-}
-
-export default Events
+export default Events;
