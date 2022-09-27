4a438d8d154b8db8519e5c4c448f71bf804165d3
Yuri <yurifdev@gmail.com>
2022-09-27 11:23:24 -0300

    Eu posso colocar JSX fora do return

--- hello-world/src/components/Events.js
+++ hello-world/src/components/Events.js
@@ -4,13 +4,36 @@ const Events = () => {
     console.log("Cliquei");
   };
 
+  const renderSomething = (x) => {
+    if(x){
+        return <p>Renderizando o X</p>
+    } else {
+        return <p>Renderizando o Y</p>
+    }
+
+  }
+
   //Se eu colocar parenteses nessa função do onClick, ela será disparada quando o navegador ler o código
   return (
     <div>
       <div>
         <button onClick={handleMyEvent}>Clique aqui</button>
       </div>
-      <button onClick={()=>console.log('Clicado de novo')}>Clique aqui também</button>
+      <div>
+        <button onClick={() => console.log("Clicado de novo")}>
+          Clique aqui também
+        </button>
+      </div>
+      <div>
+        {/* Essa logina complexa não deve existir no html */}
+        <button onClick={() => {
+            if (true) {
+                console.log("This shouldn't supost to be here");
+            }
+        }}>Mal exemplo de lógica</button>
+      </div>
+      {renderSomething(true)}
+      {renderSomething(false)}
     </div>
   );
 };
